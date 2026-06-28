c51e187066d709b7fa22ba4eaeded44a404dbeab
S7venKing <3r4n01d@gmail.com>
2026-06-28 21:29:19 +0700

    add data seed file

--- server/magic-stream-movie-server/main.go
+++ server/magic-stream-movie-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	controller "github.com/S7venKing/movie-streaming-go/server/magic-stream-movie-server/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,10 @@ func main() {
 	router := gin.Default()
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, Magic Stream Movie")
+	})
+
+	router.GET("/movies", func(c *gin.Context) {
+		controller.GetMovies()
 	})
 
 	if err := router.Run(":9080"); err != nil {
