1b8d1a633731bf79bede76dc66cfbb5f7c4bc051
S7venKing <3r4n01d@gmail.com>
2026-06-30 20:55:42 +0700

    update code

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/S7venKing/movie-streaming-go/server/magic-stream-movie-server/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,9 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, Magic Stream Movie")
 	})
+
+	routes.SetupUnProtectedRoutes(router)
+	routes.SetupProtectedRoutes(router)
 
 	if err := router.Run(":9080"); err != nil {
 		fmt.Println("Failed to start server")
